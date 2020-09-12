c86cffe1f9f573468c2cc260a2f00eb3b560923c
Connor <c.mccurrie@gmail.com>
2020-09-12 10:56:19 -0500

    first commit

---
+++ DatesComposite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DatesComposite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now;
+            // Console.WriteLine("1. {0:MMMM} {0:dd}, {0:yyyy}" , today);
+            // Console.WriteLine("2. {0:yyyy}.{0:MM}.{0:dd}" , today);
+            // Console.WriteLine("3. Day {0:dd} of {0:MMMM}, {0:yyyy}" , today);
+            // Console.WriteLine("4. Year:{0:yyyy},Month:{0:MM},Day:{0:dd}" , today);
+            // Console.WriteLine("5. {0:dddd}" , today);
+            // Console.WriteLine("6. {0:hh}:{0:mm}" , today);
+            // Console.WriteLine("7. h:{0:hh}, m:{0:mm}, s:{0:ss}" , today);
+            // Console.WriteLine("8. {0:yyyy}.{0:MM}.{0:dd}.{0:hh}.{0:mm}.{0:ss}" , today);
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"{today:yyyy}.{today:MM}.{today:dd}");
+            Console.WriteLine($"Day {today:dd} of {today:MMMM}, {today:yyyy}");
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"{today:MMMM} {today:dd}, {today:yyyy}");
+            
+
+        }
+    }
+}
---
+++ SleepData/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+namespace SleepData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+            var file = "data.txt";
+            if (resp == "1")
+            {
+              // create data file
+
+                // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine());
+
+                                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+
+                // random number generator
+                Random rnd = new Random();
+                // create file
+                StreamWriter sw = new StreamWriter("data.txt");
+                // loop for the desired # of weeks
+                while (dataDate < dataEndDate)
+                {
+                    // 7 days in a week
+                    int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++)
+                    {
+                        // generate random number of hours slept between 4-12 (inclusive)
+                        hours[i] = rnd.Next(4, 13);
+                    }
+                    // M/d/yyyy,#|#|#|#|#|#|#
+                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");                    // add 1 week to date
+                    // add 1 week to date
+                    dataDate = dataDate.AddDays(7);
+                }
+                 sw.Close();
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+                if (File.Exists(file)){
+                    StreamReader reader = new StreamReader(file);
+                    while(!reader.EndOfStream){
+                        string line = reader.ReadLine();
+                        string[] week = line.Split(',');
+                        DateTime date = DateTime.Parse(week[0]);
+                        int[] hours = Array.ConvertAll(week[1].Split('|'), int.Parse);
+                        Console.WriteLine($"Week of {date:MMM},{date: dd},{date:yyyy}");
+                        Console.WriteLine($"{"Sun", 4}{"Mon", 4}{"Tue", 4}{"Wed", 4}{"Thu", 4}{"Fri", 4}{"Sat", 4}");
+                        Console.WriteLine($"{"--", 4}{"--", 4}{"--", 4}{"--", 4}{"--", 4}{"--", 4}{"--", 4}{"--", 4}{"--", 4}");
+                        Console.WriteLine($"{hours[0], 4}{hours[1], 4}{hours[2], 4}{hours[3], 4}{hours[4], 4}{hours[5], 4}{hours[6], 4};");
+                        Console.WriteLine();
+                    }
+                }
+                    else
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+                }
+            }
+        }
+    }
